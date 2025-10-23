84c1bc3a97d4a987378e0f87bbe53053b3c0b000
Sneara0 <snearaparvin.cse@gmail.com>
2025-10-23 22:18:24 +0600

    Add components ,contact,about,actions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import  ClerkThemeProvider  from "@/components/ClerkThemeProvider";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 
 const geistSans = Geist({
@@ -26,17 +27,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+   
+   
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <ThemeProvider>
+   <ClerkThemeProvider>
+          
           <Navbar />
+          <Footer/>
           {children}
+           </ClerkThemeProvider>
+
           </ThemeProvider>
+        
         </body>
       </html>
-    </ClerkProvider>
+   
+
   );
 }
